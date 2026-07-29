4bc4485c33ccaf96715c41734ce9bf02db940f4a
Ziyanda10 <zamgobhozi10@gmail.com>
2026-07-30 00:21:13 +0200

    Badge Cabinet

---
+++ Controllers/BadgeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using SmartRecyclingRewardsSystem.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartRecyclingRewardsSystem.Controllers
+{
+    [Authorize(Roles = "Resident")]
+        public class BadgeController : Controller
+        {
+            private readonly BadgeService _badgeService = new BadgeService();
+
+         
+            public ActionResult Cabinet()
+            {
+                var residentId = User.Identity.GetUserId();
+                var cabinet = _badgeService.GetBadgeCabinet(residentId);
+                return View(cabinet);
+            }
+        }
+    
+}
---
+++ ViewModels/BadgeCabinetViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartRecyclingRewardsSystem.ViewModels
+{
+        public class BadgeCabinetViewModel
+        {
+            public int BadgeId { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string IconClass { get; set; }
+            public bool IsEarned { get; set; }
+            public DateTime? EarnedAt { get; set; }
+
+            public double ProgressCurrent { get; set; }
+            public double ProgressTarget { get; set; }
+            public int ProgressPercent { get; set; }
+            public string ProgressLabel { get; set; }
+        }
+}
